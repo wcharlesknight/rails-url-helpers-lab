39923e025963dcfaeb5d7c2d3fac26c1064dde60
Charlie Knight <williamchknight@gmail.com>
2020-12-12 16:22:07 -0800

    tests don't pass but I'm achieving the deliverable

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :edit]
   
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def edit
+    if @student.active == false
+      @student.active = true
+    else
+      @student.active = false
+    end 
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
